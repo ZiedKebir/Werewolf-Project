30be1c8b64f24bd1b7e14083a9c6d5d52996d839
ZiedKebir <zied.kebir@students.uni-mannheim.de>
2024-09-10 13:32:56 +0200

    Added Role Information for each card

--- Preparing the dataset.py
+++ Preparing the dataset.py
@@ -14,7 +14,7 @@ os.chdir("C:/Users/ziedk/OneDrive/Bureau/Data Science Projects/Werewolf-Project"
 
 # Connect to MongoDB
 client = MongoClient('mongodb://localhost:27017/')
-db = client['image_database']
+db = client['Werewolf']
 fs = gridfs.GridFS(db)
 
 
@@ -89,16 +89,16 @@ os.listdir("Images")
 """
 
 def store_image_and_augment(Image_Name:str):
-    
+    Role_Name = Image_Name.split('.')[0]
     #Open the image in read-only format.
     with open('Images/'+Image_Name, 'rb') as f:
         contents = f.read()
 
     #Now store/put the image via GridFs object.
-    fs.put(contents, filename=Image_Name)
+    fs.put(contents, filename=Image_Name,Role=Role_Name)
 
     
-    for i in range(1,11):
+    for i in range(1,100):
         augmented_image = data_augmentation(Image_Name)
         
         # Assuming img_array is your NumPy array (image data)
@@ -114,7 +114,7 @@ def store_image_and_augment(Image_Name:str):
         # Get the byte data
         img_bytes = byte_io.getvalue()
 
-        fs.put(img_bytes, filename=Image_Name+str(i))
+        fs.put(img_bytes, filename=Image_Name+str(i),Role=Role_Name)
     return None
 
 
@@ -124,7 +124,7 @@ def store_all_images_and_modif():
         print(Image_Name)
         store_image_and_augment(Image_Name)
 
-
+store_all_images_and_modif()
 
 #Get All the files stored in MongoDb
 for grid_out in fs.find():
@@ -138,3 +138,15 @@ def byte_to_image(mongodb_file_name:str):
     return Pil_Image
 
 
+## Get More Images 
+
+import numpy as np
+import os
+import PIL
+import PIL.Image
+import tensorflow as tf
+import tensorflow_datasets as tfds
+import pathlib
+dataset_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz"
+archive = tf.keras.utils.get_file(origin=dataset_url, extract=True)
+data_dir = pathlib.Path(archive).with_suffix('')
